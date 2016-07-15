9981d78aae6de3d5ebec1e7e700df5aaefccd7fc
rgpass <rgpass@gmail.com>
2016-07-15 16:07:19 +0700

    Strong progress on the About page for mobile

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ var babel = require('gulp-babel');
 var distPath = './dist';
 var scssPaths = './stylesheets/**/*.scss';
 var jsPaths = './javascripts/**/*.js';
+var imgPaths = './images/**/*.*';
+var pathsToCopy = [imgPaths];
 
 gulp.task('sass', function compileSass() {
   return gulp.src(scssPaths)
@@ -20,11 +22,16 @@ gulp.task('es6', function transpileJS() {
     .pipe(gulp.dest(distPath + '/js'));
 });
 
+gulp.task('copy-images', function copyFiles() {
+  return gulp.src(pathsToCopy)
+    .pipe(gulp.dest(distPath + '/img'));
+});
+
 gulp.task('watch', function watchForChanges() {
   gulp.watch(scssPaths, ['sass']);
   gulp.watch(jsPaths, ['es6']);
 });
 
-gulp.task('serve', ['sass', 'es6', 'watch']);
+gulp.task('serve', ['sass', 'es6', 'copy-images', 'watch']);
 
 gulp.task('default', ['serve']);
