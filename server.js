86887a408a42a7d330d9105403c72b10050ee1db
rgpass <rgpass@gmail.com>
2016-07-20 16:43:48 +0700

    Enable use of a favicon

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const favicon = require('serve-favicon');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/dist'));
+app.use(favicon(path.join(__dirname, 'dist', 'img', 'favicon.ico')));
 
 app.get('/', (req, res) => {
   res.render('about', {
