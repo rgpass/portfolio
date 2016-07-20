c2a6afc45327bb72422796b1f4736bbf532c9275
rgpass <rgpass@gmail.com>
2016-07-20 16:41:36 +0700

    Align to all ES6

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-var express = require('express');
-var app = express();
-var path = require('path');
+const express = require('express');
+const app = express();
+const path = require('path');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
