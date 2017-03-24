2384e0fe004f548f802fd4f223fd8c4ec131f564
Gerry Pass <rgpass@gmail.com>
2017-03-23 20:17:47 -0400

    Add more readable server message

--- server.js
+++ server.js
@@ -51,5 +51,5 @@ app.get('*', (req, res) => {
 });
 
 app.listen(process.env.PORT, () => {
-  console.log('Example app listening on port ' + process.env.PORT + '!');
+  console.log(`⚡⚡  Server started on port ${process.env.PORT}`);
 });
